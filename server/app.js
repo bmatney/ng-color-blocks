27d2650c831e4638799127f02a4647715b984514
Brian Matney <brianamatney@gmail.com>
2016-11-29 07:09:23 -0600

    moved colors to DataFactory

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, './public')));
 
 // Handle index file separately
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
-})
+});
 
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Listening on port: ', app.get('port'));
-});
+  });
--- server/public/scripts/controllers/gameController.js
+++ server/public/scripts/controllers/gameController.js
@@ -1,32 +1,33 @@
-colorBlocks.controller('GameController', ['$scope', function($scope) {
+colorBlocks.controller('GameController', ['$scope', 'DataFactory', function ($scope, DataFactory) {
 
-console.log('game controller running');
+  console.log('game controller running');
 
-var self = this;
-self.colors = ['red', 'blue', 'magenta', 'green', 'pink'];
+  var self = this;
+  self.colors = DataFactory;
 
-// start game
-init();
+  // start game
+  init();
 
-// resets game to the starting state
-function init() {
-  self.messageText = "";
-  self.currentColor = self.colors[randomNumber(0, self.colors.length - 1)];
-  self.colorPrompt = 'Can you find the ' + self.currentColor + ' block?'
-}
-
-// click handler for guessing colors
-self.handleInput = function(clickedColor) {
-  if(clickedColor === self.currentColor) {
-    alert('You got it!\n\nNow try another!');
-    init();
-  } else {
-    self.messageText = 'Oh no! You guessed wrong!';
+  // resets game to the starting state
+  function init() {
+    self.messageText = "";
+    self.currentColor = self.colors[randomNumber(0, self.colors.length - 1)];
+    self.colorPrompt = 'Can you find the ' + self.currentColor + ' block?';
   }
-}
 
-//UTILITY FUNCTIONS
-function randomNumber(min, max){
+  // click handler for guessing colors
+  self.handleInput = function (clickedColor) {
+    if (clickedColor === self.currentColor) {
+      alert('You got it!\n\nNow try another!');
+      init();
+    } else {
+      self.messageText = 'Oh no! You guessed wrong!';
+    }
+  };
+
+  //UTILITY FUNCTIONS
+  function randomNumber(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
-}
-}]);
+  }
+},
+]);
